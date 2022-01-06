7fc4ceb6908a4c8601a339548c6dfe51d328878c
codvatechlabs <78929919+codvatechlabs@users.noreply.github.com>
2022-01-06 20:47:30 +0530

    Update main.tf

--- Terraform/EC2_Linux/main.tf
+++ Terraform/EC2_Linux/main.tf
@@ -1,53 +1,27 @@
-/* Create new ec2 instance with below configurations*/
 
-resource "aws_instance" "ec2phpdemo1srp" {
-  //count         = 1
-  ami           = "ami-052cef05d01020f1d"
-  instance_type = "t2.micro"
-  tags = {
-    Name             = "ec2-phpdemo"
-    Created_By       = "Terraform_Automation"
-    Application_Name = "Terraform_Demo"
 
+resource "aws_instance" "ec2_terraform" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  root_block_device {
+    volume_type = var.volume_type
+    volume_size = var.volume_size
+    encrypted   = "true"
   }
+  tags                   = var.tag_values
+  key_name               = var.ec2_key_name
+  subnet_id              = var.subnet_name
 
-  key_name               = "chaitanya32q1"
-  subnet_id              = "subnet-6f19ee04"
-  
 
   user_data = <<-EOF
-                #!/bin/bash 
-                yum update -y 
-                yum install wget 
-                wget https://www.apachefriends.org/xampp-files/7.0.23/xampp-linux-x64-7.0.23-0-installer.run 
-                chmod +x xampp-linux-x64-7.0.23-0-installer.run 
-                ./xampp-linux-x64-7.0.23-0-installer.run 
-                sudo /opt/lampp/lampp start
-                sudo /opt/lampp/lampp restart
+                #!/bin/bash
+                yum update -y
+                yum install -y httpd
+                systemctl start httpd.service
+                systemctl enable httpd.service
+                echo "Welcome to EC2 advance Demo Terraform Demo!!!, I am $(hostname -f) hosted by Terraform" > /var/www/html/index.html
                 EOF
 }
-
-resource "aws_security_group" "allow_all_trafic2" {
-  name        = "allow_alltrafic2"
-  description = "allow_all_trafic inbound traffic,Allow all outbound traffic"
-
-  ingress {
-    #http
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "allow_all trafic inbound"
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "allow all traffic outbound"
-  }
-}
-
 
 
