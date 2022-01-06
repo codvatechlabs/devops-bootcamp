1b1e349fbd2cfc175d3d82366ab0543db7dcdea4
codvatechlabs <78929919+codvatechlabs@users.noreply.github.com>
2022-01-06 20:50:23 +0530

    Update variables.tf

--- Terraform/EC2_Windows/variables.tf
+++ Terraform/EC2_Windows/variables.tf
@@ -1,6 +1,38 @@
-provider "aws" {
-  region     = "ap-south-1"
-  access_key = "YOUR_ACCESS_KEY"
-  secret_key = "YOUR_SECRET_KEY"
-  version    = "~> 2.7"
+variable "instance_type" {
+  type    = string
+  default = "t2.small"
+}
+
+variable "ami_id" {
+  type    = string
+  default = "ami-052cef05d01020f1d"
+}
+
+variable "volume_type" {
+  type    = string
+  default = "gp2"
+}
+
+variable "volume_size" {
+  type    = string
+  default = "10"
+}
+
+variable "ec2_key_name" {
+  type    = string
+  default = "chaitanya32q1"
+}
+
+variable "subnet_name" {
+  type    = string
+  default = "subnet-6f19ee04"
+}
+
+variable "tag_values" {
+  type = map(any)
+  default = {
+    "Name"             = "ec2-tf-demo",
+    "Created_By"       = "Terraform_Automation",
+    "Application_Name" = "Terraform_Demo"
+  }
 }
