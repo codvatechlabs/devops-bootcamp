0a5e8a976543dcf341000b1193315d8710da65f7
codvatechlabs <78929919+codvatechlabs@users.noreply.github.com>
2022-04-23 01:22:15 +0530

    Create emp_dashboard_monitoring.sh

---
+++ CI_CD_with_Infra/emp_dashboard_monitoring.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+url='http://3.110.196.207:8080/CrudDemoWithMySql/'
+attempts=2
+timeout=5
+online=false
+
+echo "Checking status of $url."
+
+for (( i=1; i<=$attempts; i++ ))
+do
+  code=`curl -sL --connect-timeout 20 --max-time 30 -w "%{http_code}\\n" "$url" -o /dev/null`
+
+  echo "Found code $code for $url."
+
+  if [ "$code" = "200" ]; then
+    echo "Website $url is online."
+    online=true
+    break
+  else
+    echo "Website $url seems to be offline. Waiting $timeout seconds."
+    sleep $timeout
+  fi
+done
+
+if $online; then
+  echo "Monitor finished, website is online."
+  exit 0
+else
+  echo "Monitor failed, website seems to be down."
+  exit 1
+fi
