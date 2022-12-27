0e09b456bd66e8cf05c2c2f2573231f341930041
codvatechlabs <78929919+codvatechlabs@users.noreply.github.com>
2022-12-28 03:03:43 +0530

    Update deployment.sh

--- CI_CD_Scripts_without_infra/deployment.sh
+++ CI_CD_Scripts_without_infra/deployment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 systemctl stop tomcat
-url=https://devops2022.jfrog.io/artifactory/default-maven-local/CrudDemoWithMySql/CrudDemoWithMySql/0.0.1-SNAPSHOT/CrudDemoWithMySql-0.0.1-SNAPSHOT.war
-wget --user=ctldevopsnov2021@gmail.com --password=Devops@2022 -q -N $url
+url=https://ctlbootcamp.jfrog.io/artifactory/libs-snapshot-local/CrudDemoWithMySql/CrudDemoWithMySql/0.0.1-SNAPSHOT/CrudDemoWithMySql-0.0.1-SNAPSHOT.war
+wget --user=ctlsep2022@gmail.com --password=Bootcamp@1234 -q -N $url
 rm -rf /tmp/artifacts
 mkdir /tmp/artifacts
 mv CrudDemoWithMySql-0.0.1-SNAPSHOT.war CrudDemoWithMySql.war
